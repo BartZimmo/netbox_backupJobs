39be6feaca41f21dfbabbc78efae6cb165a00360
Bart Van der Biest <bart@zimmo.be>
2026-07-06 15:00:32 +0200

    Add enums and filters for grapql

--- netbox_backupjobs/graphql/enums.py
+++ netbox_backupjobs/graphql/enums.py
@@ -1,6 +1,58 @@
 import strawberry
 
-from netbox_backupjobs.choices import BackupJobStatusChoices
+from netbox_backupjobs.choices import (
+    BackupJobPlatformChoices,
+    BackupJobStatusChoices,
+    BackupJobAlgorithmChoices,
+    BackupJobStorageEncryptionEnabledChoices,
+    BackupJobEnableDeduplicationChoices,
+    BackupJobEnableDeletedVmDataRetentionChoices,
+    BackupJobEnableFullBackupChoices,
+    BackupJobEnableSyntheticFullForIncrementalChoices,
+    BackupJobEnableSyntheticFullForReverseIncrementalChoices,
+    BackupJobSyntheticFullChoices,
+    BackupJobSyntheticFullDaysChoices,
+    BackupJobSyntheticFullWeekChoices,
+    BackupJobSyntheticFullMonthChoices,
+    BackupJobFullBackupScheduleKindChoices,
+    BackupJobFullBackupDaysChoices,
+    BackupJobFullBackupWeekChoices,
+    BackupJobFullBackupMonthChoices,
+    BackupJobGFSEnableChoices,
+    BackupJobGFSWeeklyEnabledChoices,
+    BackupJobGFSWeeklyDayChoices,
+    BackupJobGFSMonthlyEnabledChoices,
+    BackupJobGFSWeekOfMonthChoices,
+    BackupJobGFSYearlyEnabledChoices,
+    BackupJobGFSMonthOfYearChoices,
+)
 
 
+BackupJobPlatformEnum = strawberry.enum(BackupJobPlatformChoices.as_enum())
 BackupJobStatusEnum = strawberry.enum(BackupJobStatusChoices.as_enum())
+BackupJobAlgorithmEnum = strawberry.enum(BackupJobAlgorithmChoices.as_enum())
+BackupJobStorageEncryptionEnabledEnum = strawberry.enum(BackupJobStorageEncryptionEnabledChoices.as_enum())
+BackupJobEnableDeduplicationEnum = strawberry.enum(BackupJobEnableDeduplicationChoices.as_enum())
+BackupJobEnableDeletedVmDataRetentionEnum = strawberry.enum(BackupJobEnableDeletedVmDataRetentionChoices.as_enum())
+BackupJobEnableFullBackupEnum = strawberry.enum(BackupJobEnableFullBackupChoices.as_enum())
+BackupJobEnableSyntheticFullForIncrementalEnum = strawberry.enum(
+    BackupJobEnableSyntheticFullForIncrementalChoices.as_enum()
+)
+BackupJobEnableSyntheticFullForReverseIncrementalEnum = strawberry.enum(
+    BackupJobEnableSyntheticFullForReverseIncrementalChoices.as_enum()
+)
+BackupJobSyntheticFullEnum = strawberry.enum(BackupJobSyntheticFullChoices.as_enum())
+BackupJobSyntheticFullDaysEnum = strawberry.enum(BackupJobSyntheticFullDaysChoices.as_enum())
+BackupJobSyntheticFullWeekEnum = strawberry.enum(BackupJobSyntheticFullWeekChoices.as_enum())
+BackupJobSyntheticFullMonthEnum = strawberry.enum(BackupJobSyntheticFullMonthChoices.as_enum())
+BackupJobFullBackupScheduleKindEnum = strawberry.enum(BackupJobFullBackupScheduleKindChoices.as_enum())
+BackupJobFullBackupDaysEnum = strawberry.enum(BackupJobFullBackupDaysChoices.as_enum())
+BackupJobFullBackupWeekEnum = strawberry.enum(BackupJobFullBackupWeekChoices.as_enum())
+BackupJobFullBackupMonthEnum = strawberry.enum(BackupJobFullBackupMonthChoices.as_enum())
+BackupJobGFSEnableEnum = strawberry.enum(BackupJobGFSEnableChoices.as_enum())
+BackupJobGFSWeeklyEnabledEnum = strawberry.enum(BackupJobGFSWeeklyEnabledChoices.as_enum())
+BackupJobGFSWeeklyDayEnum = strawberry.enum(BackupJobGFSWeeklyDayChoices.as_enum())
+BackupJobGFSMonthlyEnabledEnum = strawberry.enum(BackupJobGFSMonthlyEnabledChoices.as_enum())
+BackupJobGFSWeekOfMonthEnum = strawberry.enum(BackupJobGFSWeekOfMonthChoices.as_enum())
+BackupJobGFSYearlyEnabledEnum = strawberry.enum(BackupJobGFSYearlyEnabledChoices.as_enum())
+BackupJobGFSMonthOfYearEnum = strawberry.enum(BackupJobGFSMonthOfYearChoices.as_enum())
--- netbox_backupjobs/graphql/filters.py
+++ netbox_backupjobs/graphql/filters.py
@@ -1,21 +1,158 @@
-import strawberry
-import strawberry_django
-
+from datetime import datetime
 from typing import TYPE_CHECKING, Annotated
 
+import strawberry
+import strawberry_django
 from strawberry import ID
-from strawberry_django import FilterLookup
+from strawberry_django import DatetimeFilterLookup, StrFilterLookup
 
 from netbox.graphql.filters import NetBoxModelFilter
 
 from netbox_backupjobs.models import BackupJob
 
-# if TYPE_CHECKING:
-#     from ipam.graphql.filters import PrefixFilter
-#     from netbox.graphql.filter_lookups import IntegerArrayLookup, IntegerLookup
+if TYPE_CHECKING:
+    from ipam.graphql.filters import IPAddressFilter
+    from netbox.graphql.filter_lookups import IntegerLookup, StringArrayLookup
+    from virtualization.graphql.filters import VirtualMachineFilter
 
-#     from .enums import BackupJobStatusEnum
+    from .enums import (
+        BackupJobPlatformEnum,
+        BackupJobStatusEnum,
+        BackupJobAlgorithmEnum,
+        BackupJobStorageEncryptionEnabledEnum,
+        BackupJobEnableDeduplicationEnum,
+        BackupJobEnableDeletedVmDataRetentionEnum,
+        BackupJobEnableFullBackupEnum,
+        BackupJobEnableSyntheticFullForIncrementalEnum,
+        BackupJobEnableSyntheticFullForReverseIncrementalEnum,
+        BackupJobSyntheticFullEnum,
+        BackupJobSyntheticFullDaysEnum,
+        BackupJobSyntheticFullWeekEnum,
+        BackupJobFullBackupScheduleKindEnum,
+        BackupJobFullBackupWeekEnum,
+        BackupJobFullBackupDaysEnum,
+        BackupJobGFSEnableEnum,
+        BackupJobGFSWeeklyEnabledEnum,
+        BackupJobGFSWeeklyDayEnum,
+        BackupJobGFSMonthlyEnabledEnum,
+        BackupJobGFSWeekOfMonthEnum,
+        BackupJobGFSYearlyEnabledEnum,
+        BackupJobGFSMonthOfYearEnum,
+    )
+
 
 @strawberry_django.filter_type(BackupJob, lookups=True)
 class BackupJobFilter(NetBoxModelFilter):
-    pass
+    name: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    jobtype: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    platform: Annotated['BackupJobPlatformEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')] | None = (
+        strawberry_django.filter_field()
+    )
+    status: Annotated['BackupJobStatusEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')] | None = (
+        strawberry_django.filter_field()
+    )
+    virtual_machines: Annotated['VirtualMachineFilter', strawberry.lazy('virtualization.graphql.filters')] | None = (
+        strawberry_django.filter_field()
+    )
+    description: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    job_creation_time: DatetimeFilterLookup[datetime] | None = strawberry_django.filter_field()
+    backup_server_name: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    backup_server_ip: Annotated['IPAddressFilter', strawberry.lazy('ipam.graphql.filters')] | None = (
+        strawberry_django.filter_field()
+    )
+    backup_server_ip_id: ID | None = strawberry_django.filter_field()
+    target: StrFilterLookup[str] | None = strawberry_django.filter_field()
+
+    # Advanced settings
+    Algorithm: Annotated['BackupJobAlgorithmEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')] | None = (
+        strawberry_django.filter_field()
+    )
+    EnableDeduplication: Annotated[
+        'BackupJobEnableDeduplicationEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    StorageEncryptionEnabled: Annotated[
+        'BackupJobStorageEncryptionEnabledEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    RetainDaysToKeep: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    RetainCycles: StrFilterLookup[str] | None = strawberry_django.filter_field()
+    EnableDeletedVmDataRetention: Annotated[
+        'BackupJobEnableDeletedVmDataRetentionEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    RetainDaysToKeepDeletedVmData: StrFilterLookup[str] | None = strawberry_django.filter_field()
+
+    # Synthetic full backup settings
+    TransformFullToSyntethic: Annotated[
+        'BackupJobEnableSyntheticFullForIncrementalEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    TransformToSyntheticFull: Annotated[
+        'BackupJobEnableSyntheticFullForReverseIncrementalEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    TransformToSyntethicKind: Annotated[
+        'BackupJobSyntheticFullEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    TransformToSyntheticDays: Annotated['StringArrayLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+    SyntheticFullDayNumberInMonth: Annotated[
+        'BackupJobSyntheticFullWeekEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    SyntheticFullDayOfWeek: Annotated[
+        'BackupJobSyntheticFullDaysEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    TransformToSyntethicMonthly: Annotated[
+        'StringArrayLookup', strawberry.lazy('netbox.graphql.filter_lookups')
+    ] | None = strawberry_django.filter_field()
+
+    # Active full backup settings
+    EnableFullBackup: Annotated[
+        'BackupJobEnableFullBackupEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    FullBackupScheduleKind: Annotated[
+        'BackupJobFullBackupScheduleKindEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    FullBackupDays: Annotated['StringArrayLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+    FullBackupDayNumberInMonth: Annotated[
+        'BackupJobFullBackupWeekEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    FullBackupDayOfWeek: Annotated[
+        'BackupJobFullBackupDaysEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    FullBackupMonths: Annotated['StringArrayLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+
+    # GFS retention settings
+    EnableGFS: Annotated['BackupJobGFSEnableEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')] | None = (
+        strawberry_django.filter_field()
+    )
+    WeeklyEnabled: Annotated[
+        'BackupJobGFSWeeklyEnabledEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    WeeklyKeepBackupsFor: Annotated['IntegerLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+    WeeklyKeepBackupsOnDayOfWeek: Annotated[
+        'BackupJobGFSWeeklyDayEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    MonthlyEnabled: Annotated[
+        'BackupJobGFSMonthlyEnabledEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    MonthlyKeepBackupsFor: Annotated['IntegerLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+    MonthlyKeepBackupsWeekOfMonth: Annotated[
+        'BackupJobGFSWeekOfMonthEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    YearlyEnabled: Annotated[
+        'BackupJobGFSYearlyEnabledEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+    YearlyKeepBackupsFor: Annotated['IntegerLookup', strawberry.lazy('netbox.graphql.filter_lookups')] | None = (
+        strawberry_django.filter_field()
+    )
+    YearlyKeepBackupsOnMonthOfYear: Annotated[
+        'BackupJobGFSMonthOfYearEnum', strawberry.lazy('netbox_backupjobs.graphql.enums')
+    ] | None = strawberry_django.filter_field()
+
+    comments: StrFilterLookup[str] | None = strawberry_django.filter_field()
